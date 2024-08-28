2053d968bc2a20a2848bc6677de736e31b0bc401
Val Sylenko <vsylenko@gmail.com>
2024-08-27 22:04:52 -0400

    chore: Update dependencies and workflow launcher

--- src/main/java/ca/sylenko/temporal/DisputeResolutionWorkflowLauncher.java
+++ src/main/java/ca/sylenko/temporal/DisputeResolutionWorkflowLauncher.java
@@ -1,5 +1,7 @@
 package ca.sylenko.temporal;
 
+import java.util.UUID;
+
 import ca.sylenko.temporal.activities.CollectDocumentsActivitiesImpl;
 import ca.sylenko.temporal.activities.CreateDisputeCaseActivitiesImpl;
 import ca.sylenko.temporal.activities.GenerateReportActivitiesImpl;
@@ -16,6 +18,11 @@ import io.temporal.serviceclient.WorkflowServiceStubsOptions;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
 
+/**
+ * This class is the main entry point for the Dispute Resolution Workflow. It
+ * launches the Workflow and registers the Activities and Workflow
+ * implementations.
+ */
 public class DisputeResolutionWorkflowLauncher {
 
 	private static final String TASK_QUEUE = "dispute resolution queue";
@@ -23,6 +30,8 @@ public class DisputeResolutionWorkflowLauncher {
 	private static final String WORKDLOW_ID = "dispute-resolution-workflow-case-";
 
 	public static void main(String[] args) {
+		// Generate a random case ID if it's not provided as an argument
+		String caseId = args.length < 1 ? UUID.randomUUID().toString().substring(0, 8) : args[0];
 
 		// Use the new WorkflowServiceStubsOptions to configure and create
 		// WorkflowServiceStubs
@@ -46,7 +55,7 @@ public class DisputeResolutionWorkflowLauncher {
 
 		// A dispute resolution processing Workflow might include case number in the Workflow ID
 		WorkflowOptions workflowOptions = WorkflowOptions.newBuilder()
-				.setWorkflowId(WORKDLOW_ID + "NS-12345") // Append the case ID to the Workflow ID
+				.setWorkflowId(WORKDLOW_ID + caseId) // Append the case ID to the Workflow ID
 				.setTaskQueue(TASK_QUEUE)
 				.setWorkflowIdReusePolicy(
 						WorkflowIdReusePolicy.WORKFLOW_ID_REUSE_POLICY_ALLOW_DUPLICATE_FAILED_ONLY)  // Allow duplicate Workflow IDs only if the previous Workflow execution failed
@@ -54,6 +63,12 @@ public class DisputeResolutionWorkflowLauncher {
 
 		// Start the single Workflow execution
 		DisputeResolutionWorkflow workflow = client.newWorkflowStub(DisputeResolutionWorkflow.class, workflowOptions);
-		WorkflowClient.start(workflow::startDisputeResolution, "NS-12345"); // Pass the case ID as an argument
+
+		// Blocking start of Workflow (execution will be waiting until it's completed)
+		WorkflowClient.start(workflow::startDisputeResolution, caseId); // Pass the case ID as an argument
+
+		// Workflow will be started at this point but the call doesn't block.
+		// WorkflowClient.execute(workflow::startDisputeResolution, "NS-12346");
+
 	}
 }
--- src/main/java/ca/sylenko/temporal/activities/CollectDocumentsActivitiesImpl.java
+++ src/main/java/ca/sylenko/temporal/activities/CollectDocumentsActivitiesImpl.java
@@ -1,16 +1,12 @@
 package ca.sylenko.temporal.activities;
 
-import java.time.Duration;
-
-import io.temporal.workflow.Workflow;
-
 public class CollectDocumentsActivitiesImpl implements CollectDocumentsActivities {
     @Override
     public void collectDocuments(String caseId) {
         System.out.println("Collecting documents for case: " + caseId);
         try {
-            // Simulate document collection
-            Thread.sleep(5000);  // Sleep for 5 seconds
+            // Simulate document collection by sleeping for 5 seconds
+            Thread.sleep(5000); 
         } catch (InterruptedException e) {
             // Handle the exception as needed
             Thread.currentThread().interrupt();
